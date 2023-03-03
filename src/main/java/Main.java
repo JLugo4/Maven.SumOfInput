79e75c2fa93e5f9967c49cec740c25cabde94129
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-03 18:58:43 -0500

    I have made an sum of input method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        
         int number = getInputNumber();
-        
+
         int sum = sumOfNumbers(number);
         
         System.out.println(sum);
@@ -15,12 +15,16 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++){
+         sum = sum + i;
+        }
+        return sum;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -36,7 +40,8 @@ public class Main {
     
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
-        return 0;
+        int sum = n*(n+1)/2;
+        return sum;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
